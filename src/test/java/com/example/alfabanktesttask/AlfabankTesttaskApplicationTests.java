1e482ac66983b1f81ae33af578328a779b941cd4
ahsel21 <sik63rus@mail.ru>
2021-10-14 18:23:41 +0400

    Add tests

---
+++ src/test/java/com/example/alfabanktesttask/AlfabankTesttaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alfabanktesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfabankTesttaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
